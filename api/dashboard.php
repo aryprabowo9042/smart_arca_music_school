dd49e2349850de276f85ea922514c9f09d13da2f
aryprabowo9042 <aryprabowo9042@gmail.com>
2026-01-11 22:13:21 +0700

    Create dashboard.php

---
+++ api/dashboard.php
@@ -0,0 +1,8 @@
+<?php
+// File ini ada di folder api/dashboard.php
+// Sejajar dengan login.php
+
+echo "<h1>HALO! INI DASHBOARD ADMIN</h1>";
+echo "<p>Karena file ini sejajar dengan login, pasti bisa dibuka!</p>";
+echo "<a href='logout.php'>Logout</a>";
+?>
