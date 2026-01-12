a5cee3127522f12c850eba6cb5fa75aae675fa19
aryprabowo9042 <aryprabowo9042@gmail.com>
2026-01-12 10:13:45 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,16 +1,12 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
-$port = getenv('DB_PORT');
+$host = "alamat-host-tidb";
+$user = "alamat-user";
+$pass = "password-bapak";
+$db   = "nama-db";
 
-$conn = mysqli_init();
-// Baris ini sangat penting untuk TiDB Cloud
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+$conn = mysqli_connect($host, $user, $pass, $db);
 
-if (mysqli_connect_errno()) {
+if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
