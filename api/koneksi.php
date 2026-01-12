9d703d8ca84c8b2d02a46a2f86d0ee9498b27c10
aryprabowo9042 <aryprabowo9042@gmail.com>
2026-01-12 12:40:57 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,13 +1,25 @@
 <?php
+// AMBIL DATA INI DARI DASHBOARD TiDB CLOUD (Tombol Connect)
 $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"; 
-$user = "USERNAME_ASLI_BAPAK.root"; 
-$pass = "PASSWORD_ASLI_BAPAK"; 
+$user = "2VSxBZjDY3MNSj4.root"; // <--- INI HARUS DIGANTI DENGAN USERNAME ASLI BAPAK
+$pass = "ytvrGFzlPIBWg4eC"; // <--- INI HARUS DIGANTI DENGAN PASSWORD ASLI BAPAK
 $db   = "smart_arca"; 
 $port = 4000;
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-$real_connect = mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $conn, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
 
 if (!$real_connect) {
     die("Koneksi gagal: " . mysqli_connect_error());
