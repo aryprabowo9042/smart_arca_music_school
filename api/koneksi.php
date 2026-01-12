62a276536e903719097cca0abf27c77deb60ad36
aryprabowo9042 <aryprabowo9042@gmail.com>
2026-01-12 10:23:23 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,10 +1,12 @@
 <?php
-$host = "alamat-host-tidb";
-$user = "alamat-user";
-$pass = "password-bapak";
-$db   = "nama-db";
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"; // Sesuaikan dengan host TiDB Bapak
+$user = "data-user-bapak.root"; // Sesuaikan username asli
+$pass = "password-asli-bapak"; // Sesuaikan password asli
+$db   = "smart_arca"; // Sesuaikan nama database Bapak
+$port = 4000;
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+// Gunakan mysqli_connect dengan tambahan port jika perlu
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
