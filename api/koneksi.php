1860774fe15d547bd09b810f0fde1bee0df414f1
aryprabowo9042 <aryprabowo9042@gmail.com>
2026-01-12 10:42:07 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,19 +1,27 @@
 <?php
-// Pastikan data ini sesuai dengan yang ada di TiDB Cloud Console Bapak
+// AMBIL DATA INI DARI DASHBOARD TiDB CLOUD BAPAK
 $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"; 
-$user = "data-user-bapak.root"; 
-$pass = "password-asli-bapak"; 
-$db   = "smart_arca"; 
+$user = "2VSxBZjDY3MNSj4.root"; 
+$pass = "ytvrGFzlPIBWg4eC"; 
+$db   = "test"; 
 $port = 4000;
 
-// Membuat objek koneksi
 $conn = mysqli_init();
 
-// Mengatur SSL agar koneksi aman (Wajib untuk TiDB Cloud Serverless)
+// Pengaturan SSL (Wajib untuk TiDB Serverless)
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-// Melakukan koneksi dengan bendera MYSQLI_CLIENT_SSL
-$real_connect = mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $conn, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
 
 if (!$real_connect) {
     die("Koneksi gagal: " . mysqli_connect_error());
