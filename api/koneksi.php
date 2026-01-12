cc4a9b923fc9c13ead69c380043a250348a14c9c
aryprabowo9042 <aryprabowo9042@gmail.com>
2026-01-12 11:33:37 +0700

    Update koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,27 +1,13 @@
 <?php
-// AMBIL DATA INI DARI DASHBOARD TiDB CLOUD BAPAK
 $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"; 
-$user = "2VSxBZjDY3MNSj4.root"; 
-$pass = "ytvrGFzlPIBWg4eC"; 
-$db   = "test"; 
+$user = "USERNAME_ASLI_BAPAK.root"; 
+$pass = "PASSWORD_ASLI_BAPAK"; 
+$db   = "smart_arca"; 
 $port = 4000;
 
 $conn = mysqli_init();
-
-// Pengaturan SSL (Wajib untuk TiDB Serverless)
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-
-// Melakukan koneksi
-$real_connect = mysqli_real_connect(
-    $conn, 
-    $host, 
-    $user, 
-    $pass, 
-    $db, 
-    $port, 
-    NULL, 
-    MYSQLI_CLIENT_SSL
-);
+$real_connect = mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
 
 if (!$real_connect) {
     die("Koneksi gagal: " . mysqli_connect_error());
