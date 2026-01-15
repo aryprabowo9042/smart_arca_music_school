f404977e99d30a28794acd69b341ce4ebbe245ec
aryprabowo9042 <aryprabowo9042@gmail.com>
2026-01-16 06:27:26 +0700

    Update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,21 +1,14 @@
 <?php
-// api/logout.php
 session_start();
 session_unset();
 session_destroy();
 
-// Hapus semua cookie dengan jalur '/' agar bersih total di semua folder
+// Hapus semua Cookie login
 $expire = time() - 3600;
 setcookie('user_role', '', $expire, '/');
 setcookie('user_id', '', $expire, '/');
 setcookie('user_username', '', $expire, '/');
 
-// Gunakan JavaScript sebagai pengaman tambahan jika Header PHP gagal
-echo "
-<script>
-    localStorage.clear();
-    sessionStorage.clear();
-    window.location.replace('/'); // Paksa kembali ke halaman utama (root)
-</script>
-";
+// Lempar ke halaman depan
+header("Location: index.php");
 exit();
